c10580c59f273c762fe6f6c0175301b1853cc801
jorisnorvaisas <joris1337q@gmail.com>
2022-09-09 12:26:09 +0200

    commit

---
+++ functions!/script.js
@@ -0,0 +1,56 @@
+// oppgave 1
+
+/* function sum(tall1, tall2, tall3) {
+    return(tall1 + tall2 + tall3)
+}
+
+document.write(sum(2,6,7))
+*/
+
+// oppgave 2
+
+/* var number1 = parseInt(prompt("skriv inn et tall"))
+var number2 = parseInt(prompt("skriv inn et andre tall"))
+var number3 = parseInt(prompt("skriv inn et tredje tall"))
+
+function sum(tall1, tall2, tall3) {
+    return(tall1 + tall2 + tall3)
+}
+
+document.write(sum(number1,number2,number3))
+*/
+
+// oppgave 3
+
+/*function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+var minimum = parseInt(prompt("skriv inn minimum"))
+var maximum = parseInt(prompt("skriv inn maximum"))
+
+document.write(Math.round(getRandomNumber(minimum,maximum)))
+*/
+
+// oppgave 4
+
+
+// oppgave 5
+
+/*function sum(tall1, tall2) {
+    return (tall1) + " + " + (tall2) + " = " + (tall1 + tall2) + "<br>"
+}
+
+document.writeln(sum(3,5))
+document.writeln(sum(8,1))
+
+function puttogether(tall1, tall2) {
+    return (tall1) + " + " + (tall2) + " = " + tall1 + tall2 + "<br>"
+}
+
+document.writeln(puttogether(3,5))
+document.writeln(puttogether(8,1))
+*/
+
+// oppgave 6
+
