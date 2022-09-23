cb7facd0436e115d2188b45bff033f2b1f69163c
jorisnorvaisas <joris1337q@gmail.com>
2022-09-23 13:50:26 +0200

    commit

---
+++ arrays/script.js
@@ -0,0 +1,77 @@
+// oppgave 1
+
+/* var myarray = [6, 2, 12, "abc", 5, 9]
+
+function findFirstElement(array) {
+    return array[0]
+}
+
+document.write(findFirstElement(myarray))
+*/
+
+// oppgave 2
+
+/* var myarray = [6, 2, 12, "abc", 5, 9]
+
+function findLastElement(array) {
+    return array[array.length - 1]
+}
+
+document.write(findLastElement(myarray))
+*/
+
+// oppgave 3
+
+/* var myarray = [6, 2, 12, 83, 5, 9]
+
+var sum = 0
+var multiplySum = 0
+
+function sumAndMultiply(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i]
+        multiplySum = array.reduce((accumulator, current) => accumulator*current,1);
+
+    }
+
+    return sum + "<br>" + multiplySum
+}
+
+document.write(sumAndMultiply(myarray))
+*/
+
+// oppgave 4
+
+/* var myarray = [6, 2, 12, 83, 5, 9]
+var myarray2 = [7, 1, 2, 6, 4, 12]
+
+function arraySum(array1, array2) {
+  var result = [];
+  var ctr = 0;
+  var x = 0;
+
+ while (ctr < array1.length && ctr < array2.length) 
+  {
+    result.push(array1[ctr] + array2[ctr]);
+    ctr++;
+  }
+
+ if (ctr === array1.length) 
+ {
+    for (x = ctr; x < array2.length; x++)   {
+      result.push(array2[x]);
+    }
+  } 
+  else
+  {
+  for (x = ctr; x < array1.length; x++) 
+    {
+      result.push(array1[x]);
+    }
+  }
+  return result;
+}
+
+document.write(arraySum(myarray, myarray2));
+*/
